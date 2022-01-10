c60ccc5ee5d31e85d80b7f8f7991cc93500f2407
RitaSgomes <94627735+RitaSgomes@users.noreply.github.com>
2022-01-10 10:28:30 -0300

    Update ExclusaoLogica.Repository.java

--- ExclusaoLogica.Repository.java
+++ ExclusaoLogica.Repository.java
@@ -1,3 +1,9 @@
 public class usuarioRepositoty extends CrudRepository<Usuario, Long>{
+  //Consulta usuarios ativos
+  @Query("SELECT u FROM User u WHERE u.status = 1")
+  Collection<Usuarios> findAllActiveUsers();
+  //Consulta usuarios inativos
+  @Query("SELECT u FROM User u WHERE u.status = 0")
+  Collection<Usuarios> findAllInActiveUsers();
   
-  private list<UsuarioAtivo> findbyStatus(Status);
+}
